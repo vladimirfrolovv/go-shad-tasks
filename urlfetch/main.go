8b3d4f2edbc9e82c22b091a728366cce75c09ec9
new laptop <vladimir.frolov.99@mail.ru>
2024-06-13 19:10:52 +0300

    done url fetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,24 @@
 
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
+	argsWithoutProg := os.Args[1:]
+	for _, url := range argsWithoutProg {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Print("fetch: Get " + url + ": unsupported protocol scheme \"\"")
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		fmt.Println(string(body))
+	}
 
 }
