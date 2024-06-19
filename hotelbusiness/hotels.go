aed956d0de665d966fe57650e259c381a4c46be0
new laptop <vladimir.frolov.99@mail.ru>
2024-06-19 20:36:27 +0300

    hotelbuisness done

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,8 @@
 
 package hotelbusiness
 
+import "sort"
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +15,34 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	start := make(map[int]int)
+	for _, g := range guests {
+		for j := g.CheckInDate; j <= g.CheckOutDate; j++ {
+			start[j]++
+		}
+		start[g.CheckOutDate]--
+	}
+
+	load := make([]Load, len(start))
+	i := 0
+	for k, v := range start {
+		load[i].StartDate = k
+		load[i].GuestCount = v
+		i++
+	}
+	sort.Slice(load[:], func(i, j int) bool {
+		return load[i].StartDate < load[j].StartDate
+	})
+
+	tmp := make([]Load, len(load))
+	j, k := 0, 0
+	for j < len(load) {
+		tmp[k] = load[j]
+		for j+1 < len(load) && load[j].GuestCount == load[j+1].GuestCount {
+			j++
+		}
+		j++
+		k++
+	}
+	return tmp[:k]
 }
