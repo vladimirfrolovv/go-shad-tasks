e3bd1755bbbf5f2f743c8f82a51253a6dbfdf048
new laptop <vladimir.frolov.99@mail.ru>
2024-06-20 18:55:40 +0300

    utf8 done

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -3,5 +3,15 @@
 package reverse
 
 func Reverse(input string) string {
-	return ""
+	runes := []rune(input)
+	i, j := 0, len(runes)-1
+	tmp := rune(2)
+	for i < j {
+		tmp = runes[i]
+		runes[i] = runes[j]
+		runes[j] = tmp
+		i++
+		j--
+	}
+	return string(runes)
 }
--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -3,5 +3,21 @@
 package spacecollapse
 
 func CollapseSpaces(input string) string {
-	return ""
+	runes := []rune(input)
+	l, r, countSpace := 0, 0, 0
+	for r < len(runes) {
+		if runes[r] == ' ' || runes[r] == '\t' || runes[r] == '\n' || runes[r] == '\r' {
+			countSpace++
+			if countSpace == 1 {
+				runes[l] = ' '
+				l++
+			}
+		} else {
+			runes[l] = runes[r]
+			l++
+			countSpace = 0
+		}
+		r++
+	}
+	return string(runes[:l])
 }
