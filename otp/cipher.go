5d8730eddaffdbd29dbab117b8591542b043ac93
new laptop <vladimir.frolov.99@mail.ru>
2024-08-29 17:39:58 +0300

    otp

--- otp/cipher.go
+++ otp/cipher.go
@@ -6,10 +6,49 @@ import (
 	"io"
 )
 
+type CipherReader struct {
+	r    io.Reader
+	prng io.Reader
+}
+
+func (c *CipherReader) Read(p []byte) (n int, err error) {
+
+	n, err = c.r.Read(p)
+
+	key := make([]byte, n)
+	c.prng.Read(key)
+	for i := 0; i < n; i++ {
+		p[i] ^= key[i]
+	}
+	if err != nil {
+		return n, err
+	}
+	return n, nil
+}
+
 func NewReader(r io.Reader, prng io.Reader) io.Reader {
-	panic("implement me")
+	return &CipherReader{r: r, prng: prng}
 }
 
 func NewWriter(w io.Writer, prng io.Reader) io.Writer {
-	panic("implement me")
+	return &CipherWriter{w: w, prng: prng}
+}
+
+type CipherWriter struct {
+	w    io.Writer
+	prng io.Reader
+}
+
+func (c *CipherWriter) Write(p []byte) (n int, err error) {
+	key := make([]byte, len(p))
+	_, err = c.prng.Read(key)
+	if err != nil {
+		return n, err
+	}
+	ciphertext := make([]byte, len(p))
+	for i := 0; i < len(p); i++ {
+		ciphertext[i] = p[i] ^ key[i]
+	}
+
+	return c.w.Write(ciphertext)
 }
